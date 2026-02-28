ce5cea6197cba1f1d944a8e761a73ce42f7ecd96
elysewelsh <elysecwelsh@gmail.com>
2026-02-28 14:30:16 -0500

    measurements and ingredients display side-by-side

--- src/pages/RecipeDetailPage.jsx
+++ src/pages/RecipeDetailPage.jsx
@@ -33,22 +33,43 @@ function RecipeDetailPage() {
             );
         }
     }
+    const measurements = [];
+    for(let i=1; i < 21; i++ ) {
+        const measure = recipe[`strMeasure${i}`];
+        if (measure && measure !== "") {
+            measurements.push(
+                <li key={i}>{measure}</li>
+            );
+        }
+    }
     console.log(ingredients);
+    console.log(measurements);
     
     // const product = products.find(prod => prod.id == id)
 
     return (
         // <AppProviders>
         !recipe.strMeal ? <></> :
-            <div>
-                <h2>{recipe.strMeal}</h2>
-                <div>
-                    <div>Details for Product: {recipe.idMeal}</div>
-                    <div>Name: {recipe.strMeal}</div>
+            <div className="text-center">
+                <h2 className="font-bold">{recipe.strMeal}</h2>
+                <h3 className="font-bold mt-10">Ingredients</h3>
+                <div className="flex flex-row gap-3 mt-3">
+                    <div>
+                        <h4 className="mb-2 font-bold">Measurement</h4>
+                        <ul>
+                        {measurements}
+                        </ul>
+                    </div>
+                    <div>
+                        <h4 className="mb-2 font-bold">Ingredient</h4>
+                        <ul>
+                        {ingredients}
+                        </ul>
+                    </div>
+
                 </div>
-                <ul>
-                    {ingredients}
-                </ul>
+                <h3 className="font-bold">Instructions</h3>
+
             </div>
 
     )
